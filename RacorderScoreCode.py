d6436f9e91839edf604f42ee9d7256247a9c1893
ericshao3 <laosan6@163.com>
2013-05-22 09:56:51 +0800

    change IRcorder

---
+++ RacorderScoreCode.py
@@ -0,0 +1,154 @@
+__author__ = 'stone'
+#encoding:utf-8
+#File:RecorderScore.py
+import web
+from web.contrib.template import render_mako
+
+import globalDefine
+import traceback
+import configData
+from configData import getConfig
+from logHelper import getLogger
+
+import re
+import base64
+
+import json
+
+import urlparse
+import OrderDomainHandler
+
+web.config.debug = False
+
+urls = (
+        '/RacorderScore/(.*)','RacorderScore'
+        )
+
+app = web.application(urls,globals(),autoreload=True)
+session = web.session.Session(app,web.session.DiskStore('sessions'),
+initializer={'session_grpid':'','session_usrid':'','session_loginned':'','session_pwd':''})
+
+def session_hook():
+    web.ctx.session = session
+
+app.add_processor(web.loadhook(session_hook))
+
+allowed = (
+    (getConfig('allowedUser1','UserName','str'),getConfig('allowedUser1','Password','str')),
+    (getConfig('allowedUser2','UserName','str'),getConfig('allowedUser2','Password','str'))
+    )
+
+# input_encoding and output_encoding is important for unicode
+# template file. Reference:
+# http://www.makotemplates.org/docs/documentation.html#unicode
+render = render_mako(
+        directories = ['templates'],
+        input_encoding = 'utf-8',
+        output_encoding = 'utf-8',
+        )
+
+class RacorderScore():
+        def POST(self,filename):
+            pass
+
+        def GET(self,filename):
+            try:
+                logger = getLogger()
+                logger.debug("start rqscoscr Page GET response")
+
+                globalDefine.globalOrderInfoErrorlog = "No Error"
+
+                #TODO: open the auth in future.also need purview.
+                #            authreq = checkUserAuth(web)
+                #
+                #            if authreq:
+                #                web.header('WWW-Authenticate','Basic realm="Auth example"')
+                #                web.ctx.status = '401 Unauthorized'
+                #                logger.debug("no right HTTP_AUTHORIZATION")
+                #                return render.error(error = web.ctx.status)
+
+                #TODO: when None happen?
+                if filename is '' or filename is None:
+                    return render.error(error = 'no filename')
+                else:
+                    return render.RacorderScoreView(outdicrqscoscr=getTestData())
+
+            except :
+                logger.error("exception occur, see the traceback.log")
+                #异常写入日志文件.
+                f = open('traceback.txt','a')
+                traceback.print_exc()
+                traceback.print_exc(file = f)
+                f.flush()
+                f.close()
+            else:
+                pass
+            finally:
+                pass
+
+
+def getTestData():
+    subitem = [{"subitemname":"0","subitemvalue":"0"},{"subitemname":"1","subitemvalue":"1"},{"subitemname":"2","subitemvalue":"2"}]
+    dicrqscoscr = []
+    recordequestion = {"itemID":"1","itemDesc":"开场白(真诚问候，专业身份与多美滋的关系 15%)","itemPerc":"10","subitems":subitem}
+
+    dicrqscoscr.append(recordequestion)
+
+    recordequestion = {"itemID":"2","itemDesc":"建议（评估结果 40%）","itemPerc":"10","subitems":subitem}
+    dicrqscoscr.append(recordequestion)
+
+    recordequestion = {"itemID":"3","itemDesc":"开场白,(表达同理心,对妈妈/孕妇的关心 自然真诚 15%)","itemPerc":"10","subitems":subitem}
+
+    dicrqscoscr.append(recordequestion)
+    recordequestion = {"itemID":"4","itemDesc":"开场白,(告之目的和利益，明确告知致电的目的或利益 2，并确认生日，预产期15%)","itemPerc":"10","subitems":subitem}
+    dicrqscoscr.append(recordequestion)
+
+
+    recordequestion = {"itemID":"5","itemDesc":"开场白(真诚问候，专业身份与多美滋的关系 15%)","itemPerc":"10","subitems":subitem}
+
+    dicrqscoscr.append(recordequestion)
+
+    recordequestion = {"itemID":"6","itemDesc":"建议（评估结果 40%）","itemPerc":"10","subitems":subitem}
+    dicrqscoscr.append(recordequestion)
+
+    recordequestion = {"itemID":"7","itemDesc":"开场白,(表达同理心,对妈妈/孕妇的关心 自然真诚 15%)","itemPerc":"10","subitems":subitem}
+
+    dicrqscoscr.append(recordequestion)
+    recordequestion = {"itemID":"8","itemDesc":"开场白,(告之目的和利益，明确告知致电的目的或利益 2，并确认生日，预产期15%)","itemPerc":"10","subitems":subitem}
+    dicrqscoscr.append(recordequestion)
+
+    recordequestion = {"itemID":"9","itemDesc":"开场白(真诚问候，专业身份与多美滋的关系 15%)","itemPerc":"10","subitems":subitem}
+
+    dicrqscoscr.append(recordequestion)
+
+    recordequestion = {"itemID":"10","itemDesc":"建议（评估结果 40%）","itemPerc":"10","subitems":subitem}
+    dicrqscoscr.append(recordequestion)
+
+    recordequestion = {"itemID":"11","itemDesc":"开场白,(表达同理心,对妈妈/孕妇的关心 自然真诚 15%)","itemPerc":"10","subitems":subitem}
+
+    dicrqscoscr.append(recordequestion)
+    recordequestion = {"itemID":"12","itemDesc":"开场白,(告之目的和利益，明确告知致电的目的或利益 2，并确认生日，预产期15%)","itemPerc":"10","subitems":subitem}
+    dicrqscoscr.append(recordequestion)
+
+    dicrqscoscrS = json.dumps(dicrqscoscr)
+
+    return dicrqscoscrS
+
+def checkUserAuth(inWeb):
+    logger = getLogger()
+    auth = inWeb.ctx.env.get('HTTP_AUTHORIZATION')
+    authreq = False
+    if auth is None:
+        authreq = True
+    else:
+        auth = re.sub('^Basic ','',auth)
+        username,password = base64.decodestring(auth).split(':')
+        if (username,password) in allowed:
+            logger.debug("has right HTTP_AUTHORIZATION")
+            pass
+        else:
+            authreq = True
+    return authreq
+
+if __name__ == "__main__":
+    app.run()
--- configObjData.py
+++ configObjData.py
@@ -2,6 +2,7 @@ __author__ = 'stone'
 #coding=UTF-8
 
 from configobj import ConfigObj
+import  RacorderClient
 import os
 
 def getConfigPage():
@@ -46,10 +47,11 @@ def getRecorderConfigPage():
     """get the recorder page config file"""
 
     #just return json.
-    configPage = ConfigObj('recorderPagesConf.conf')
+    #configPage = ConfigObj('recorderPagesConf.conf')
 
     #return a config list
-#    configPage = ConfigObj()
+    #configPage = ConfigObj()
+    configPage=getRecorderConfigPage();
 #
 #    #
 #    configPage['hasShowScore'] = 'yes'
@@ -144,6 +146,145 @@ def getTestData():
     dicrqscoscrS = json.dumps(dicrqscoscr)
 
     return dicrqscoscrS
+
+
+
+# 创建录音打分的页面
+def getRecorderConfigPage():
+    tRacorderQuestions = RacorderClient.GetRacorderQuestion();
+    if tRacorderQuestions is None or len(tRacorderQuestions)==0 :
+        return render.error(error = 'no filename')
+    else:
+        recorderScore = len(tRacorderQuestions)
+
+
+
+    #return a config list
+        configPage = ConfigObj()
+
+
+
+        SBaseName = 'Q' # 序号
+
+        i = 0
+        for item in tRacorderQuestions:
+            itemDesc = item["itemDesc"];   # 问题描述
+            itemID = item["itemID"]
+            i=i+1
+
+            Sname = SBaseName + str(i)
+            configPage[Sname] = {}
+            configPage[Sname]['sequence'] = {}
+            configPage[Sname]['sequence']['controlText'] =itemID
+            configPage[Sname]['sequence']['controlType'] = 'label'
+            configPage[Sname]['sequence']['controlCss'] = 'label label-normal'
+            configPage[Sname]['sequence']['dataType'] = 'input'
+            configPage[Sname]['sequence']['controlName'] = 'lblSEQUENCE'+str(i)
+            configPage[Sname]['sequence']['mustHave'] = 'no'
+
+
+
+            configPage[Sname]['question'] = {}
+            configPage[Sname]['question']['controlText'] =itemDesc
+            configPage[Sname]['question']['controlType'] = 'label'
+            configPage[Sname]['question']['controlCss'] = 'label label-normal'
+            configPage[Sname]['question']['dataType'] = 'input'
+            configPage[Sname]['question']['controlName'] = 'lblQUESTION'+str(i)
+            configPage[Sname]['question']['mustHave'] = 'no'
+
+
+            configPage[Sname]['pinji'] = {}
+            configPage[Sname]["pinji"]['controlText'] ='评级'
+            configPage[Sname]["pinji"]['controlType'] = 'label'
+            configPage[Sname]["pinji"]['controlCss'] = 'label label-normal'
+            configPage[Sname]["pinji"]['dataType'] = 'input'
+            configPage[Sname]["pinji"]['controlName'] = 'lblPINJI'+str(i)
+            configPage[Sname]["pinji"]['mustHave'] = 'no'
+
+
+            configPage[Sname]['class'] = {}
+            configPage[Sname]["class"]['controlType'] = 'input'
+            configPage[Sname]["class"]['controlShowType'] = 'radio'
+            configPage[Sname]["class"]['controlName'] = 'radioCLASS'+str(i)
+            configPage[Sname]['class']["hasRealDataValue"] = '2'
+
+
+
+            configPage[Sname]['class']['radio'] = {}
+            subitems = item["subitems"];   #问题答案子类
+            radioBaseName = 'radio'
+            j = 0
+            for itemj in subitems:
+                j=j+1;
+                text = itemj["subitemname"]  # 显示值
+                value = itemj["subitemvalue"] # value值
+                sectionRadio = {
+                    'titleType': 'label',
+                    'titleCss': 'radio inline',
+                    'inDataType': 'label',
+                    'inDataCss': 'label',
+                    'inDataText':text,
+                    'inDataValue': value,
+                    'inDataID': 'rl_answer'+str(j),
+                    }
+                radioName = radioBaseName + str(j)
+
+                configPage[Sname]['class']['radio'][radioName] = sectionRadio
+
+            configPage[Sname]['zhanbi'] = {}
+            configPage[Sname]['zhanbi']['controlText'] = '占比'
+            configPage[Sname]['zhanbi']['controlType'] = 'label'
+            configPage[Sname]['zhanbi']['controlCss'] = 'label label-normal'
+            configPage[Sname]['zhanbi']['controlName'] = 'label label-normal'
+
+
+            itemPerc = item["itemPerc"] #百分比
+            configPage[Sname]['percent'] = {}
+            configPage[Sname]['percent']['controlText'] = itemPerc +"%"
+            configPage[Sname]['percent']['controlType'] = 'label'
+            configPage[Sname]['percent']['controlCss'] = 'label label-normal'
+            configPage[Sname]['percent']['controlName'] = 'lblPERCENT'+str(i)
+
+
+            configPage[Sname]['defen'] = {}
+            configPage[Sname]['defen']['controlText'] = "得分"
+            configPage[Sname]['defen']['controlType'] = 'label'
+            configPage[Sname]['defen']['controlCss'] = 'label label-normal'
+            configPage[Sname]['defen']['controlName'] = 'lblDEFEN'+str(i)
+            configPage[Sname]['defen']['mustHave'] = 'no'
+
+            configPage[Sname]['score'] = {}
+            configPage[Sname]['score']['controlType'] = 'input'
+            configPage[Sname]['score']['controlCss'] = 'input-mini'
+            configPage[Sname]['score']['controlShowType'] = 'text'
+            configPage[Sname]['score']['controlName'] = 'txtSCORE'+str(i)
+            configPage[Sname]['score']['controlID'] = 'idSCORE'+str(i)
+            configPage[Sname]['score']['placeholder'] = '得分'
+
+            configPage[Sname]['memo'] = {}
+            configPage[Sname]['memo']['controlText'] = "备注"
+            configPage[Sname]['memo']['controlType'] = 'label'
+            configPage[Sname]['memo']['controlCss'] = 'label label-normal'
+            configPage[Sname]['memo']['controlName'] = 'lblMEMO'+str(i)
+            configPage[Sname]['memo']['mustHave'] = 'no'
+
+            configPage[Sname]['memoText'] = {}
+            configPage[Sname]['memoText']['controlType'] = 'input'
+            configPage[Sname]['memoText']['controlCss'] = 'input-large'
+            configPage[Sname]['memoText']['controlShowType'] = 'text'
+            configPage[Sname]['memoText']['controlName'] = 'txtMemo'+str(i)
+            configPage[Sname]['memoText']['controlID'] = 'idMemo'+str(i)
+            configPage[Sname]['memoText']['hasRealDataValue'] = ''
+            configPage[Sname]['memoText']['placeholder'] = '备注'
+
+
+        print configPage
+
+    return configPage
+
+
+
+
 
 def getConfigFile(inFileName):
     """get the Static config file"""
