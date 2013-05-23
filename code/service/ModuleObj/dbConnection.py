5576e1784a22e6996925f0a854147686449357ec
lbzhao28 <lbzhao28@gmail.com>
2013-05-23 18:13:09 +0800

    fix the mssql connection bug.

--- code/service/ModuleObj/dbConnection.py
+++ code/service/ModuleObj/dbConnection.py
@@ -5,6 +5,10 @@ def dbConnect():
     db = web.database(dbn=getConfig('db','dbname','str'),db=getConfig('db','dbservice','str'),user=getConfig('db','dbuser','str'),pw=getConfig('db','dbpwd','str'))
     return db
 
+def dbMSSqlConnect():
+    db = web.database(dbn=getConfig('dbMSSql','dbname','str'),host=getConfig('dbMSSql','dbhost','str'),db=getConfig('dbMSSql','db','str'),user=getConfig('dbMSSql','dbuser','str'),pw=getConfig('dbMSSql','dbpwd','str'))
+    return db
+
 def dbSqliteConnect():
     db = web.database(dbn=getConfig('dbSqlite','dbname','str'),db=getConfig('dbSqlite','dbfile','str'))
     return db
